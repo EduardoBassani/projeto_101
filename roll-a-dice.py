5823d2c9ab145cdf3a1a7db36d72f41b4a83432f
EduardoBassani <88408579+EduardoBassani@users.noreply.github.com>
2023-02-23 19:03:57 -0300

    Add files via upload

---
+++ roll-a-dice.py
@@ -0,0 +1,26 @@
+import random 
+
+resposta='sim'
+
+while(resposta=='sim'):
+    nu = random.randint(1,6)
+    if nu==1:
+        print("-----")
+        print("     ")
+        print("  0  ")
+        print("     ")
+        print("-----")
+
+    if nu==2
+        print("-----")
+        print("0    ")
+        print("     ")
+        print("    0")
+        print("-----")
+
+    if nu==3
+        print()
+
+
+    resposta = str(input("você quer rolar o dado?"))
+    print("\n")
